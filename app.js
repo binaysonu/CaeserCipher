5b6084bdbb54204611db1d3eea212b3896c9ec5c
feature_swag <155911688+binaysonu@users.noreply.github.com>
2025-03-02 17:26:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { Pool } = require('pg');
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -9,15 +8,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const db = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
-
 
 // Caesar Cipher Function
 function caesarCipher(text, shift, encrypt = true) {
@@ -37,19 +27,16 @@ app.get("/", (req, res) => {
 });
 
 // Encrypt Text
-app.post("/encrypt", async (req, res) => {
+app.post("/encrypt", (req, res) => {
     const { text, shift } = req.body;
     const encryptedText = caesarCipher(text, parseInt(shift), true);
-
-    await db.query("INSERT INTO messages (text, shift, encrypted_text) VALUES ($1, $2, $3)", [text, shift, encryptedText]);
     res.render("result", { result: encryptedText, action: "Encrypted" });
 });
 
 // Decrypt Text
-app.post("/decrypt", async (req, res) => {
+app.post("/decrypt", (req, res) => {
     const { text, shift } = req.body;
     const decryptedText = caesarCipher(text, parseInt(shift), false);
-    
     res.render("result", { result: decryptedText, action: "Decrypted" });
 });
 
@@ -57,3 +44,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
